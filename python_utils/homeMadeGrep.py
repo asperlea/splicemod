42f8f2e18e3649bee23ce977f040082e89164b13
Adriana Sperlea <adriana.sperlea@gmail.com>
2015-02-19 23:10:22 -0800

    Updated all scripts.

--- python_utils/homeMadeGrep.py
+++ python_utils/homeMadeGrep.py
@@ -9,23 +9,27 @@ def main():
         print "Need input file to grep in and input file containing what to grep for"
         exit(1)
     words = open(sys.argv[1], "r")
-    text = open(sys.argv[2], "w")
+    text = open(sys.argv[2], "r")
 
     # hash text
-    text = {}
+    textHash = {}
+    wtf = ""
     while True:
-        next_4_lines = list(islice(inFile, 4))
+        next_4_lines = list(islice(text, 4))
         if not next_4_lines:
             break
 
         header = next_4_lines[0].split()
         ID = header[0]
-        sequence = next_4_lines[1].strip() + next_4_lines[2].strip() + next_4_lines[3].strip()
-        text[sequence] = ID
-
+        sequence = next_4_lines[1].strip().upper() + next_4_lines[2].strip().upper() + next_4_lines[3].strip().upper()
+        textHash[sequence] = ID
+        
     for line in words:
         word = line.strip()
-        if word in text:
-            print word, text[word]
+        pos = word.find("TTAATTAA")
+        word = word[:pos]
+
+        if word in textHash:
+            print word, textHash[word]
 
 main()
